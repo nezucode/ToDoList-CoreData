121d0f0d528258d65f30a6c4b0d97c3892e0a3bb
nezucode <intann89.in@gmail.com>
2023-09-15 08:08:41 +0700

    ToDoList CoreData CRUD

---
+++ ToDoList CoreData/ToDoListItem/ToDoListItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoListItem+CoreDataClass.swift
+//  ToDoList CoreData
+//
+//  Created by Intan on 14/09/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoListItem)
+public class ToDoListItem: NSManagedObject {
+
+}
